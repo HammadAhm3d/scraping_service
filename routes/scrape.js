f4dca1f15dba5685d408b458d07a248d493d4028
HammadAhm3d <66493429+HammadAhm3d@users.noreply.github.com>
2024-06-06 01:43:41 +0300

    removed unnecessary middleware

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { scrapeData } = require("../controllers/scrapeController");
-const authMiddleware = require("../middlewares/auth");
 const router = express.Router();
 
 router.get("/scrape", scrapeData);
